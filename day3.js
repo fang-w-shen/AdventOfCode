837f227fd3b572b44a2507f4acd9273c246b4160
fang-w-shen <fang-wen.shen@tuftandneedle.com>
2021-12-17 01:32:44 -0600

    Day 3-PART1 Solution

--- day3.js
+++ day3.js
@@ -9,8 +9,28 @@ console.log('')
 //***********START***********
 
 //***********PART1
+function getBinary(str) {
+    let val = 0;
+    str.split('').reverse().map((e) => parseInt(e)).forEach((e, i) => {
+        if (e === 1) {
+            val += e * (2 ** i)
+        }
+    });
+    return val
+}
 
-console.log('DAY3-PART1')
+let binary = new Array(arr[0].length).fill(0)
+arr.forEach((element, index) => {
+    binary.forEach((e, i) => {
+        binary[i] += parseInt(element[i])
+    })
+})
+let gammaBinary = binary.map((e) => e > 500 ? 1 : 0).join('')
+let epsilonBinary = binary.map((e) => e < 500 ? 1 : 0).join('')
+let gamma = getBinary(gammaBinary)
+let epsilon = getBinary(epsilonBinary)
+
+console.log(gamma * epsilon, 'DAY3-PART1')
 
 //***********PART2
 
